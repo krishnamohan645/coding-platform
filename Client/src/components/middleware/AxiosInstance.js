5f84b50109b357c2bd2a97f209ada3d4fd0e56c5
citadelstech-it <krishnaguduri08@gmail.com>
2026-04-13 19:54:34 +0530

    groq

--- Client/src/components/middleware/AxiosInstance.js
+++ Client/src/components/middleware/AxiosInstance.js
@@ -1,14 +1,13 @@
 import axios from "axios";
 
-
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://coding-platform.up.railway.app/api",
   withCredentials: true,
 });
 
 axiosInstance.interceptors.request.use(
   (config) => {
-
     if (config.data instanceof FormData) {
       delete config.headers["Content-Type"];
     } else {
@@ -18,7 +17,7 @@ axiosInstance.interceptors.request.use(
   },
   (error) => {
     return Promise.reject(error);
-  }
+  },
 );
 
 export default axiosInstance;
