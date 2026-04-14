608422dca5b558d1ab4ee98c0f8cfe77cc820cf1
citadelstech-it <krishnaguduri08@gmail.com>
2026-04-14 18:18:55 +0530

    update backend url and add cors fix

--- Client/src/components/middleware/AxiosInstance.js
+++ Client/src/components/middleware/AxiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "http://localhost:5000/api",
-  baseURL: "https://coding-platform.up.railway.app/api",
+  baseURL: "http://localhost:5000/api",
+  // baseURL: "https://coding-platform.up.railway.app/api",
   withCredentials: true,
 });
 
