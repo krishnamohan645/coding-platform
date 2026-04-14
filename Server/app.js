7761d734d1448fa1e3346ef9033f29edfa97c14f
citadelstech-it <krishnaguduri08@gmail.com>
2026-04-14 18:31:23 +0530

    cors error

--- Server/app.js
+++ Server/app.js
@@ -9,11 +9,9 @@ require("dotenv").config();
 const app = express();
 
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "http://localhost:3000",
-    "https://coding-platform-kohl-two.vercel.app",
-  ],
+  origin: process.env.CORS_ORIGIN 
+    ? process.env.CORS_ORIGIN.split(",") 
+    : ["http://localhost:5173", "https://coding-platform-kohl-two.vercel.app"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept"],
