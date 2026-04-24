6fa63d15063ec9ab7723ce59a923e7e6c692f771
citadelstech-it <krishnaguduri08@gmail.com>
2026-04-24 12:17:27 +0530

    cors

--- Server/app.js
+++ Server/app.js
@@ -8,16 +8,27 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.CORS_ORIGIN 
-    ? process.env.CORS_ORIGIN.split(",") 
-    : ["http://localhost:5173", "https://coding-platform-kohl-two.vercel.app"],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept"],
-}));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN
+      ? process.env.CORS_ORIGIN.split(",")
+      : [
+          "http://localhost:5173",
+          "https://coding-platform-kohl-two.vercel.app",
+          "https://judge029.p.rapidapi.com/submissions",
+        ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "Accept",
+    ],
+  }),
+);
 
-// app.options("*", cors()); // handle preflight for all routes
+app.options("*", cors()); // handle preflight for all routes
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -33,7 +44,10 @@ app.use("/api/contents", require("./api/routes/content.routes"));
 app.use("/api/problem", require("./api/routes/problem.routes"));
 app.use("/api/user", require("./api/routes/user.routes"));
 app.use("/api/auth", require("./api/routes/auth.routes"));
-app.use("/api/practiceProblems", require("./api/routes/practiceProblems.routes"));
+app.use(
+  "/api/practiceProblems",
+  require("./api/routes/practiceProblems.routes"),
+);
 app.use("/api/userActivity", require("./api/routes/userActivity.routes"));
 app.use("/api/ai", require("./api/routes/ai.routes"));
 app.use(require("./api/middlewares/errorHandler"));
